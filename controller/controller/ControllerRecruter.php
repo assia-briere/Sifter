6220545fcfe906c24ea808ac227cde0775625644
assia-briere <124631638+assia-briere@users.noreply.github.com>
2023-03-16 13:36:40 +0100

    last update

---
+++ controller/controller/ControllerRecruter.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__DIR__) . "/Model/Recreteur.php"; 
+class ControlleR extends Recruteur{
+
+
+}
---
+++ controller/index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="./stylesheets/indexstyle.css">
+
+  <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.3.0/css/all.css">
+  <link rel="icon" type="image/x-icon" href="/img/logo.png">
+  <title>Document</title>
+</head>
+
+<body>
+  <header class="header">
+    <nav class="navbar">
+      <!-- LOGO -->
+      <div class="logo">
+        <img class="img-logo" src="/img/logo.png" alt="logo">
+        <a class="a-logo " href="#">Rec<span class="span-log">Me</span></a>
+      </div>
+      <div class="left-nav">
+        <div class="log">
+          <a class="login" href="./view/Login.php">Login</a>
+          <a href="./view/Choix.php" class="get-start">Register</a>
+        </div>
+        <!-- NAVIGATION MENU -->
+        <ul class="nav-links">
+
+          <input type="checkbox" id="checkbox_toggle" />
+          <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+          <div class="menu">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#pages">pages</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact Us</a>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div class="contain">
+    <div class="costum-hero">
+      <div class="hero-left flex-column">
+        <div class="hero-subtit flex-row">
+          <div class="hero-subtitle-box"></div>
+          <p class="hero-sub-p">Welcome to Recrute Me</p>
+        </div>
+        <div class="hero-title-wrap">
+          <h1 class="hero-title-h1">
+            The Best Value for <span class="highlight"> Business </span>with perfect Grouwth
+          </h1>
+          <p class="hero-sub-p2">We understand our customer’s needs and hence our work approach & systems will tend to
+            know the project result.</p>
+        </div>
+      </div>
+      <div class="hero-right">
+      </div>
+
+    </div>
+    <!--WELCOME PAGE-->
+    <div class="welcome-class">
+      <div class="welcome-left ">
+        <img src="./img/secend.jpg" alt="blog-image">
+        <div class="blog-left">
+          <h2 class="hero-title-h1">Prenez de meilleures décisions avec les services en col blanc</h2>
+          <p class="hero-sub-p2">Surmontez les défis et saisissez les opportunités dans ce domaine de la transformation
+            du secteur des entreprises avec nous.</p>
+          <a class="simple-link " href="/contactus">Get in Touch</a>
+        </div>
+      </div>
+      <div class="welcome-right">
+        <div class="costum-welcome">
+          <div class="flex-column">
+            <div class="hero-subtit flex-row">
+              <div class="hero-subtitle-box"></div>
+              <p class="hero-sub-p">Qui Sommes Nous</p>
+            </div>
+            <div class="hero-title-wrap">
+              <h1 class="hero-title-h1">
+                Nous sommes l'agence la <span class="highlight"> plus avancée </span>du marché
+              </h1>
+              <p class="hero-sub-p2">Nous sommes profondément dévoués à la croissance et au succès à long terme de nos
+                clients et de nos employés. Nous nous engageons à produire une valeur mesurée et des résultats.</p>
+              <p class="hero-sub-p2">Surmontez les défis et saisissez les opportunités en cette ère de transformation du
+                secteur des entreprises avec nous.</p>
+            </div>
+            <div class="intro-point flex-colmun">
+              <div class="intro-point-item ">
+                <img
+                  src="https://assets.website-files.com/626785f200042235dafbf097/627229932f75a44d2debb19b_blue-tick-mark.svg"
+                  alt="intro-item">
+                <h3 class="intro-point-title">Stratégie d'entreprise et marketing</h3>
+              </div>
+              <div class="intro-point-item">
+                <img
+                  src="https://assets.website-files.com/626785f200042235dafbf097/627229932f75a44d2debb19b_blue-tick-mark.svg"
+                  alt="intro-item">
+                <h3 class="intro-point-title">L' excellence en tout</h3>
+              </div>
+            </div>
+            <div class="welcome-section">
+              <div class="welcome-item flex-row">
+                <div class="welcome-item-icon">
+                  <img
+                    src="https://assets.website-files.com/626785f200042235dafbf097/62722d95a38f7fd770701f89_welcome-book-image.svg"
+                    alt="welcome-icon">
+                </div>
+                <div class="welcome-item-title flex-column">
+                  <h4 class="welcome-title-h4">Expérience</h4>
+                  <p class="welcome-p">Leaders sur le marché avec créativité et succès.</p>
+                </div>
+              </div>
+              <div class="welcome-item flex-row">
+                <div class="welcome-item-icon">
+                  <img
+                    src="https://assets.website-files.com/626785f200042235dafbf097/62722d95a38f7fd770701f89_welcome-book-image.svg"
+                    alt="welcome-icon">
+                </div>
+                <div class="welcome-item-title flex-column">
+                  <h4 class="welcome-title-h4">Expérience</h4>
+                  <p class="welcome-p">Leaders sur le marché avec créativité et succès.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h6>About</h6>
+          <p class="text-justify">vqrnvjùvjuqbvjubvDV nvjvncnlkqnckanckenclkneckneckckqckecknqzc</p>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Categories</h6>
+          <ul class="footer-links">
+            <li><a href="#">C</a></li>
+            <li><a href="#">UI Design</a></li>
+            <li><a href="#">PHP</a></li>
+            <li><a href="#">Java</a></li>
+            <li><a href="#">Android</a></li>
+            <li><a href="#">Templates</a></li>
+          </ul>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Quick Links</h6>
+          <ul class="footer-links">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#/contact/">Contact Us</a></li>
+            <li><a href="#">Contribute</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Sitemap</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+            <a href="#">Sifter</a>.
+          </p>
+        </div>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a class="twitter" href="#"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a class="dribbble" href="#"><i class="fa-brands fa-dribbble"></i></a></li>
+            <li><a class="linkedin" href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer -->
+
+
+
+</body>
+
+</html>
